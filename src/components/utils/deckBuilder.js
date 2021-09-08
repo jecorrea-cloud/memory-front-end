fce357c67dae76a5784547ebd27a5b6e8d593d71
joaquin <jecorrea@protonmail.com>
2021-09-08 19:06:09 -0400

    emojis added

--- src/components/utils/deckBuilder.js
+++ src/components/utils/deckBuilder.js
@@ -3,15 +3,15 @@ const CARD_NUMBER = 20;
 
 export default () => {
 
-    const fontAwesomeClasses= ['icon-2', 'icon-3']
+    const emojis= ['icon-2', 'icon-3']
 
     let cards = [];
 
     while (cards.length < CARD_NUMBER)
     {
-        const index = Math.floor(Math.random() * fontAwesomeClasses.legth)
+        const index = Math.floor(Math.random() * emojis.length)
         const card = {
-            icon: fontAwesomeClasses.splice(index, 1)[0],
+            icon: emojis.splice(index, 1)[0],
             guessed : false,
         }
 
