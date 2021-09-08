550ad19f617671b6ec154626461c90b8fe2efb17
joaquin <jecorrea@protonmail.com>
2021-09-08 17:59:28 -0400

    cards partially displaying

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './Board.css';
+import Card from "./Card"
 
 export default class Board extends Component {
     render() {
-        const cards =['🙉','🚺','😭','💌','🗯','🍂','❌','👌','🗿']
+        const cards =[1,2,3,4,5]
         return (
             <div className="board">
-                {cards.map(card=><span>{card}</span>)}
+                {cards.map(card=><Card></Card>)}
             </div>
         )
     }
