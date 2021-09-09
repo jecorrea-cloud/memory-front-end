c392f281adf179901be5dedfca2b886714d2c2d3
joaquin <jecorrea@protonmail.com>
2021-09-09 16:09:52 -0400

    added flipping effect

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import "./Card.css"
+import FlipCard from 'react-flipcard-2'
 
 export default class Card extends Component {
     render() {
         return (
             <div className="card">
-                <i className={`fa ${this.props.icon} fa-5x`}></i>
+                <FlipCard>
+                    <div className="back"></div>
+                    <div className="front"> <i className={`fa ${this.props.icon} fa-5x`}></i> </div>
+                </FlipCard>
             </div>
         )
     }
